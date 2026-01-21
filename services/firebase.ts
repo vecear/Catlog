0bfd7eef0e3bc4057614d2ab315cb9104aa8d0c7
vecear <vecear@gmail.com>
2026-01-21 14:05:04 +0800

    feat: Initialize Firebase app and export Firestore and Auth instances.

--- services/firebase.ts
+++ services/firebase.ts
@@ -5,7 +5,7 @@ import { getAuth } from "firebase/auth";
 // Firebase configuration
 // keys are technically public in a client-side app
 const firebaseConfig = {
-    apiKey: "AIzaSyCm7KdUyGJLr7SnSqBP7R1NsJlvKPOzGXk",
+    apiKey: "AIzaSyBJENv_Vhy3KFJtcDVzG4qmWmcCr389nsc",
     authDomain: "catlog-690c4.firebaseapp.com",
     projectId: "catlog-690c4",
     storageBucket: "catlog-690c4.firebasestorage.app",
